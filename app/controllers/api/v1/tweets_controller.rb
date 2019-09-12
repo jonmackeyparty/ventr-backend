4fd390716d08068cfa93cbff30130e572d78f18e
Jon Mackey <jon@jonmackey.party>
2019-09-12 16:21:47 -0400

    update controllers to access user acct, nest routes under user

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::TweetsController < ApplicationController
 
+  before_action :set_user
+
   def index
-    @tweets = Tweet.all
-    render json: @tweets
+    render json: @user.tweets
   end
 
   def create
-    @tweet = Tweet.new(tweet_params)
+    @tweet = @user.tweets.build(tweet_params)
     if @tweet.save
       render json: @tweet
     else
@@ -14,17 +15,11 @@ class Api::V1::TweetsController < ApplicationController
     end
   end
 
-  def show
-    @tweet = Tweet.find(params[:id])
-    render json: @tweet
-  end
-
-  def destroy
-    @tweet = Tweet.find(params[:id])
-    @tweet.destroy
-  end
-
   private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 
   def tweet_params
     params.require(:tweet).permit(:user_id, :content, :date)
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :responses
-      resources :selected_tweets
-      resources :tweets
-      resources :users
+      resources :users do
+        resources :tweets
+        resources :selected_tweets
+        resources :responses
+      end  
     end
   end
 
