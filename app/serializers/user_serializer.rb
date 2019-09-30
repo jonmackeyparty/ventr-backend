142232833f91b357650f5f402a2428e2a0107afa
Jon Mackey <jon@jonmackey.party>
2019-09-29 23:30:54 -0400

    add username to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :location, :bio
+  attributes :id, :username, :location, :bio, :avi_url
 
   has_many :tweets
   has_one :selected_tweet
-  
+
 end
